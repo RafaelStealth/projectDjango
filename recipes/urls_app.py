c84c158dc81a61772b681b2f09ab2b42567889ee
Rafael Vieira / Stealth <123310895+RafaelStealth@users.noreply.github.com>
2024-04-16 19:00:58 -0300

    update 1604

--- recipes/urls_app.py
+++ recipes/urls_app.py
@@ -2,7 +2,9 @@ from django.urls import path
 # from recipes.views import home, sobre
 from . import views
 
+app_name = 'recipes'
+
 urlpatterns = [
-    path('', views.home),
-    path('reciepes/<int:id>', views.recipe),
+    path('', views.home, name="home"),
+    path('recipes/<int:id>', views.recipe, name="recipe"),
 ]
