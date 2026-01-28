55a1d91113cd3f180764000e7ada8a2d22882bfd
Rafael Vieira <123310895+RafaelStealth@users.noreply.github.com>
2026-01-28 19:43:53 -0300

    Add 'sobre' route with simple view to URLs
    Introduced a new path 'sobre/' in the project's URL configuration, mapping it to a basic view that returns 'Hello, World!'. This provides a simple endpoint for testing or demonstration purposes.

--- projeto/urls.py
+++ projeto/urls.py
@@ -15,8 +15,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.http import HttpResponse
 from django.urls import path
+
+
+# o request recebe a requisição feita pelo usuário exemplo site.com/sobre é uma requisição
+def my_view(request):
+    return HttpResponse("Hello, World!")    
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('sobre/',my_view),
+
 ]
